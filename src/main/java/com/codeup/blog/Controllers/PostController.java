c8517592ee2f172b00cd216cd3a5400c470ffe39
Michael Carranza <michael.j.carranza@gmail.com>
2019-02-27 16:57:03 -0600

    added edit and update code

--- src/main/java/com/codeup/blog/Controllers/PostController.java
+++ src/main/java/com/codeup/blog/Controllers/PostController.java
@@ -8,26 +8,27 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
-    private final PostRepository PostDao;
-    public PostController(PostRepository PostDao) {
-        this.PostDao = PostDao;
+    private final PostRepository postDao;
+    public PostController(PostRepository postDao) {
+        this.postDao = postDao;
     }
 
     @GetMapping("/posts")
     public String all(Model model){
 
-        Iterable<Post> posts = PostDao.findAll();
+        Iterable<Post> posts = postDao.findAll();
         model.addAttribute("posts", posts);
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public String show(@PathVariable long id, Model model){
-
-        Post post = new Post(id, "Hello World!", "This is my first post about JavaScript, yay!" );
+        Post post = postDao.findOne(id);
         model.addAttribute("post", post);
         return "posts/show";
     }
+
+
     @GetMapping("/posts/create")
     public String showForm(){
         return "posts/create";
@@ -39,7 +40,7 @@ public class PostController {
             @RequestParam(name = "body") String body
     ){
         Post post = new Post(title, body);
-        PostDao.save(post);
+        postDao.save(post);
         return "redirect:/posts";
 
     }
@@ -49,4 +50,37 @@ public class PostController {
 //        return "posts/show";
 //    }
 
-}
+    @GetMapping("/posts/{id}/edit")
+    public String edit(@PathVariable long id, Model model){
+        Post post = postDao.findOne(id);
+        model.addAttribute("post", post);
+        return "posts/edit";
+    }
+    @PostMapping("/posts/create")
+    public String edit(
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "body") String body
+    ){
+        Post post = new Post(title, body);
+        postDao.save(post);
+        return "redirect:/posts";
+
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String update(
+            @PathVariable long id,
+            @RequestParam (name= "title") String title,
+            @RequestParam (name= "body") String body,
+            Model model) {
+        Post post = postDao.findOne(id);
+        post.setTitle(title);
+        post.setBody(body);
+
+        postDao.save(post);
+        return "redirect:/posts/" + id;
+
+    }
+
+
+}//END OF EVERYTHING
--- src/main/java/com/codeup/blog/Posts/PostRepository.java
+++ src/main/java/com/codeup/blog/Posts/PostRepository.java
@@ -1,5 +1,8 @@
 package com.codeup.blog.Posts;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository <Post, Long> {
+    @Query(value="SELECT * FROM posts ORDER BY RAND() LIMIT 1", nativeQuery=true)
+    Post getRandom();
 }
